8407377c360ae1f749839574887410aa78ea5814
SeoungMinHwang <117965731+SeoungMinHwang@users.noreply.github.com>
2024-03-13 01:50:05 +0900

    UserProfile Entity 정의

--- src/main/java/com/lolpee/server/domain/user/entity/User.java
+++ src/main/java/com/lolpee/server/domain/user/entity/User.java
@@ -1,2 +1,14 @@
-package com.lolpee.server.domain.user.entity;public class User {
+package com.lolpee.server.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users") // 엔티티가 매핑될 DB 테이블 이름 지정
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
 }
